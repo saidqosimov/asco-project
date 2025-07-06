292ad3b794d038af6b46e1d6ca3c47d207597f58
saidqosimov <said.qosimov1847@gmail.com>
2025-07-06 18:26:55 +0500

    language with header

--- src/main/java/com/ascoproject/ascoproject/config/SwaggerConfig.java
+++ src/main/java/com/ascoproject/ascoproject/config/SwaggerConfig.java
@@ -5,22 +5,36 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(
-    info = @Info(
-        title = "Your API Title",
-        version = "v1",
-        description = "This is the API documentation for your project."
-    ),
-    security = @SecurityRequirement(name = "bearerAuth") // Apply globally
+        info = @Info(
+                title = "Your API Title",
+                version = "v1",
+                description = "This is the API documentation for your project."
+        ),
+        security = @SecurityRequirement(name = "bearerAuth") // Apply globally
 )
 @SecurityScheme(
-    name = "bearerAuth",
-    type = SecuritySchemeType.HTTP,
-    scheme = "bearer",
-    bearerFormat = "JWT" // Optional: just for description
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT" // Optional: just for description
 )
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://asco.up.railway.app").url("http://localhost")
+
+                ));
+    }
+
 }
