e46a7dcf4a95468c2676563ab816dbc08c384505
saidqosimov <said.qosimov1847@gmail.com>
2025-08-21 20:36:38 +0500

    checkTest

--- src/main/java/com/ascoproject/ascoproject/controller/TestController.java
+++ src/main/java/com/ascoproject/ascoproject/controller/TestController.java
@@ -1,16 +1,18 @@
 package com.ascoproject.ascoproject.controller;
 
+import com.ascoproject.ascoproject.service.JwtService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/v1/test")
+@RequestMapping("/api/v1/checkToken")
 public class TestController {
     @GetMapping
     public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hello World. Karochche successful");
+        return ResponseEntity.ok("true");
     }
 }
--- src/main/java/com/ascoproject/ascoproject/service/JwtService.java
+++ src/main/java/com/ascoproject/ascoproject/service/JwtService.java
@@ -45,7 +45,7 @@ public class JwtService {
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    private boolean isTokenExpired(String token) {
+    public boolean isTokenExpired(String token) {
         return extractExpired(token).before(new Date());
     }
 
